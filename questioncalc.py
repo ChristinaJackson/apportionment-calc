a983ff5975c46194c881ad23fc3a88a983e9b46f
Christina Jackson <christinajackson@Christinas-MacBook-Air.local>
2019-09-06 09:38:53 -0700

    Added if statement to the OSWESTRY calc.

--- questioncalc.py
+++ questioncalc.py
@@ -1,13 +1,20 @@
 
+score1 = int(input("What is the first DASH, NDI, Roland Morris or Oswestry Score?   "))
+score2 = int(input("What is the second score?   "))
+injury1percent = None
+injury2 = None
 
 
-
-def questions():
-    score1 = int(input("What is the first DASH, NDI, Roland Morris or Oswestry Score?   "))
-    score2 = int(input("What is the second score?   "))
+def questions(score1, score2):
     injury1 = score1/score2
+    global injury1percent
     injury1percent = round(injury1 * 100)
+    global injury2
     injury2 = abs(round(injury1percent - 100))
-    return print(injury1percent, "% can be attributed to injury 1 and", injury2, "% can be attributed to injury 2")
 
-questions()
+
+if score1 < score2:
+    questions(score1, score2)
+    print(injury1percent, "% can be attributed to injury 1 and", injury2, "% can be attributed to injury 2")
+else:
+    print("Not valid")
