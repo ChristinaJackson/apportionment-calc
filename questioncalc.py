7d9287671312c534a32ab6701114058b4614ca22
Christina Jackson <christinajackson@Christinas-MacBook-Air.local>
2019-09-06 15:29:04 -0700

    Added exceptions and checks for int inputs

--- questioncalc.py
+++ questioncalc.py
@@ -1,20 +1,32 @@
 
-score1 = int(input("What is the first DASH, NDI, Roland Morris or Oswestry Score?   "))
-score2 = int(input("What is the second score?   "))
+score1 = (input("What is the first DASH, NDI, Roland Morris or Oswestry Score?   "))
+try:
+    score1 =int(score1)
+except ValueError:
+    print("Please enter a numerical value")
+    exit()
+
+score2 = (input("What is the second score?   "))
+try:
+    score2 =int(score2)
+except ValueError:
+    print("Please enter a numerical value")
+    exit()
+
 injury1percent = None
-injury2 = None
+injury2percent = None
 
 
 def questions(score1, score2):
     injury1 = score1/score2
     global injury1percent
     injury1percent = round(injury1 * 100)
-    global injury2
-    injury2 = abs(round(injury1percent - 100))
+    global injury2percent
+    injury2percent = abs(round(injury1percent - 100))
 
 
 if score1 < score2:
     questions(score1, score2)
-    print(injury1percent, "% can be attributed to injury 1 and", injury2, "% can be attributed to injury 2")
+    print(injury1percent, "% can be attributed to injury 1 and", injury2percent, "% can be attributed to injury 2")
 else:
-    print("Not valid")
+    print("Score 2 must be greater than score 1")
