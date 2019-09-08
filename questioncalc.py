be2e9977f4bf72d0c1a53124948fc06a1e2c7ece
Christina Jackson <christinajackson@Christinas-MacBook-Air.local>
2019-09-07 20:29:24 -0700

    Cleaned up code.

--- questioncalc.py
+++ questioncalc.py
@@ -1,21 +1,4 @@
 
-score1 = (input("What is the first DASH, NDI, Roland Morris or Oswestry Score?   "))
-try:
-    score1 =int(score1)
-except ValueError:
-    print("Please enter a numerical value")
-    exit()
-
-score2 = (input("What is the second score?   "))
-try:
-    score2 =int(score2)
-except ValueError:
-    print("Please enter a numerical value")
-    exit()
-
-injury1percent = None
-injury2percent = None
-
 
 def questions(score1, score2):
     injury1 = score1/score2
@@ -24,9 +7,25 @@ def questions(score1, score2):
     global injury2percent
     injury2percent = abs(round(injury1percent - 100))
 
+def bothscores():
+    score1 = 0
+    score2 = 0
+    while True:
+        try:
+            score1 = int(input("What is the first DASH, NDI, Roland Morris or Oswestry Score?   "))
+            score2 = int(input("What is the second score?   "))
+            if score1 < score2:
+                questions(score1, score2)
+                print(injury1percent, "% can be attributed to injury 1 and", injury2percent,
+                      "% can be attributed to injury 2")
+                break
+            else:
+                print("Score 2 must be greater than score 1 consider using a different method")
+                break
+        except ValueError:
+            print("Make sure to enter a numerical value! ")
 
-if score1 < score2:
-    questions(score1, score2)
-    print(injury1percent, "% can be attributed to injury 1 and", injury2percent, "% can be attributed to injury 2")
-else:
-    print("Score 2 must be greater than score 1")
+#loop returns to score one, instead score two... figure that out
+
+
+bothscores()
