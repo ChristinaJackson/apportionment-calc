c88fb04b994d3b849defd82af13c0102f240953a
ChristinaJackson <chirochristina@gmail.com>
2019-09-09 11:52:49 -0700

    Split the while loop to allow for ValueErrors on each input

--- TreatmentTimeCalc.py
+++ TreatmentTimeCalc.py
@@ -1,18 +1,24 @@
 
 def residual():
-    while True:
-        try:
-            prevremaining = int(input("How many visits were remaining on first treatment plan?  "))
-            estvisits = int(input("How many visits are estimated to treat subsequent injury?  "))
-            if prevremaining < estvisits:
-                effects = round(prevremaining/estvisits * 100)
-                print(effects, "% of the patient's residual effects were pre-existing from the first accident.")
-                break
-            else:
-                print("Score 2 must be greater than score 1 consider using a different method")
-                break
-        except ValueError:
-            print("Make sure to enter a numerical value! ")
+    if prevremaining < estvisits:
+        effects = round(prevremaining/estvisits * 100)
+        print(effects, "% of the patient's residual effects were pre-existing from the first accident.")
+    else:
+        print("Score 2 must be greater than score 1 consider using a different method")
 
+
+while True:
+    try:
+        prevremaining = int(input("How many visits were remaining on first treatment plan?  "))
+        break
+    except ValueError:
+        print("Make sure to enter a numerical value! ")
+
+while True:
+    try:
+        estvisits = int(input("How many visits are estimated to treat subsequent injury?  "))
+        break
+    except ValueError:
+        print("Make sure to enter a numerical value! ")
 
 residual()
